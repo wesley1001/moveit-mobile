9b443d13a1b4226e0f8085edd918f0d638177333
Muraleekrishna <muraleekrishnagc@gmail.com>
2015-12-04 00:07:00 +0530

    Add workout url and slack name to user

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,11 @@
 export default class User {
   constructor(params) {
+    this.id = params.id;
     this.email = params.email;
     this.name = params.name;
     this.gravatar = params.gravatar;
     this.status = params.activity_status;
+    this.slackUserName = params.slack_user_name;
     this.activities = {
       duration: params.duration,
       amount: params.amount

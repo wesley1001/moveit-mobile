94f8bd5f83756352788177dd6b74d218fb12541b
Peter Lazar <peterlazar1993@gmail.com>
2015-12-15 18:18:20 +0530

    script to build production apk

--- build_release.sh
+++ build_release.sh
@@ -1,7 +1,4 @@
-mkdir -p android/app/src/main/assets
-react-native bundle --platform android --dev false --entry-file index.android.js \
-  --bundle-output android/app/src/main/assets/index.android.bundle \
-  --assets-dest android/app/src/main/res/
+./setup.sh
 cd android && ./gradlew assembleRelease
 cp ./app/build/outputs/apk/app-release.apk ../
 cd ../

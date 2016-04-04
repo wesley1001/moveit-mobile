1890cfca8065635ced6755524d468614dead23f5
Jayakrishnan M K <jayakrishnanmk07@gmail.com>
2016-04-04 17:47:36 +0530

    Implement service to fetch charity data from spreadsheet
    https://trello.com/c/wjthoHP0/28-user-sbat-view-the-charity-contributions-done-per-month-by-the-company

--- src/services/GetCharityData.js
+++ src/services/GetCharityData.js
@@ -1,12 +1,19 @@
 class GetCharityData {
-	getData(){
-		var CHARITY_DATA = [
-			{amount: '41,234', month: 'January', year: '2015', name: 'Charity:Water', description: 'Cool description that was so hard to come up with. Hence I ended up writing some random text that dosent make much text' ,imageSource: 'https://s3.amazonaws.com/moveit-multunus/charity_resources/March2015/GiveIndia_education_march_2015.png'},
-			{amount: '41,234', month: 'February',  year: '2015', name: 'Charity:Water', description: 'Cooler description hat was so hard to come up with. Hence I ended up writing some random text that dosent make much text',imageSource: 'https://s3.amazonaws.com/moveit-multunus/charity_resources/April2015/charity_water+Apr+2015.png'},
-			{amount: '21,234', month: 'March',  year: '2015', name: 'Charity:Water', description: 'Coolest description hat was so hard to come up with. Hence I ended up writing some random text that dosent make much text',imageSource: 'https://s3.amazonaws.com/moveit-multunus/charity_resources/April2015/charity_water+Apr+2015.png'}
-		];
-		return CHARITY_DATA ;
-	}
+  getData(){
+    var CHARITY_DATA_ARRAY = [];
+    var spreadsheetID = "1f1tFr7y62QS7IRWOC2nNR5HWsTO99Bna3d0Xv1PTPiY";
+    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+    fetch(url).then((response) => response.json())
+      .then((responseText) => {
+        entry = responseText.feed.entry;
+        entry.forEach(function(element){
+          CHARITY_DATA_ARRAY.push({amount: element.gsx$charityamount.$t, month: element.gsx$month.$t, year: element.gsx$year.$t, name: element.gsx$charityname.$t, description: element.gsx$charitydesc.$t, imageSource: element.gsx$charityimage.$t });
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+    return CHARITY_DATA_ARRAY;
+  }
 }
-
 export default new GetCharityData();
